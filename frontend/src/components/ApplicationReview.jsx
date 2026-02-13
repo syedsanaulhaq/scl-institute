e9f20cebe47df7f9f658b277c80513c4d3d45095
SCL Developer <developer@sclinstitute.com>
2026-02-13 14:44:39 +0500

    Fix: Close Modal Body div properly in ApplicationReview modal

--- frontend/src/components/ApplicationReview.jsx
+++ frontend/src/components/ApplicationReview.jsx
@@ -925,6 +925,7 @@ const ApplicationReview = () => {
                                     <li>Login portal: http://localhost:3000/student/login</li>
                                 </ul>
                             </div>
+                        </div>
 
                         {/* Modal Footer */}
                         <div className="bg-gray-50 px-6 py-4 border-t flex gap-3">
