4e0931b091f54c213263367bf11f1e3cadd8e9da
SCL Developer <developer@sclinstitute.com>
2026-02-24 11:26:11 +0500

    chore: Clean up working directory

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,9 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 33061,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'root',
+    database: process.env.DB_NAME || 'scl_institute',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
