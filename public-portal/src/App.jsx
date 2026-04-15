c27c1381eeafa125c5c6c09794ed58e55dacd128
SCL Developer <developer@sclinstitute.com>
2026-04-15 14:48:46 +0500

    feat: redirect localhost:7777 root to /student-registration

--- public-portal/src/App.jsx
+++ public-portal/src/App.jsx
@@ -1,5 +1,5 @@
 import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import StudentAdmissionForm from './components/StudentAdmissionForm';
 import RegistrationLanding from './components/RegistrationLanding';
 import TeacherRegistrationForm from './components/TeacherRegistrationForm';
@@ -8,7 +8,7 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<RegistrationLanding />} />
+                <Route path="/" element={<Navigate to="/student-registration" replace />} />
                 <Route path="/student-registration" element={<StudentAdmissionForm />} />
                 <Route path="/teacher-registration" element={<TeacherRegistrationForm />} />
                 <Route path="*" element={<RegistrationLanding />} />
